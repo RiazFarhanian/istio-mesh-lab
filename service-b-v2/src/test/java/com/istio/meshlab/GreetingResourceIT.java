79418fa5908f6dacae7211f6f0f91e276c60f113
Riaz Farhanian <riaz.farhanian@gmail.com>
2025-08-04 09:55:31 -0400

    Initialize Service-A, Service-B-V1, Service-B-V2

---
+++ service-b-v2/src/test/java/com/istio/meshlab/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.istio.meshlab;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
